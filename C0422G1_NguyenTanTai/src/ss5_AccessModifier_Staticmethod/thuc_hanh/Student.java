10efe27dfd668dc948cf187c533a3b46871570ae
unknown <you@example.com>
2022-06-16 17:23:59 +0700

    refactoring

--- C0422G1_NguyenTanTai/src/ss5_AccessModifier_Staticmethod/thuc_hanh/Student.java
+++ C0422G1_NguyenTanTai/src/ss5_AccessModifier_Staticmethod/thuc_hanh/Student.java
@@ -1,6 +1,14 @@
 package ss5_AccessModifier_Staticmethod.thuc_hanh;
 
 public class Student {
+    public class Cent{
+        private String name ;
+
+        public Cent(String name) {
+            this.name = name;
+        }
+    }
+    Cent c = new Cent("hello");
     private int rollno;
     private String name ;
     private static String college = "BBDIT";
@@ -14,4 +22,10 @@ public class Student {
     void display(){
         System.out.println(rollno +" "+name + " " + college);
     }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
 }
+
